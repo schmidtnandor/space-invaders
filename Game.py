66f8d1a64770393a1e49af6f7bf06995b9dbaeec
schmidtnandor <schmidt.nandor@students.jedlik.eu>
2026-03-11 08:36:55 +0100

    game init rewritten + player shows on screen

--- Game.py
+++ Game.py
@@ -1,45 +1,24 @@
 import pygame
+from player import Player
 
 
 class Game:
-
-    # pygame setup
-    pygame.init()
-    screen = pygame.display.set_mode((1280, 720))
-    clock = pygame.time.Clock()
-    running = True
-    dt = 0
-
-    player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
-    while running:
-        # poll for events
-        # pygame.QUIT event means the user clicked X to close your window
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        # fill the screen with a color to wipe away anything from last frame
-        screen.fill("purple")
-
-        pygame.draw.circle(screen, "red", player_pos, 40)
-
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            player_pos.y -= 300 * dt
-        if keys[pygame.K_s]:
-            player_pos.y += 300 * dt
-        if keys[pygame.K_a]:
-            player_pos.x -= 300 * dt
-        if keys[pygame.K_d]:
-            player_pos.x += 300 * dt
-
-        # flip() the display to put your work on screen
-        pygame.display.flip()
-
-        # limits FPS to 60
-        # dt is delta time in seconds since last frame, used for framerate-
-        # independent physics.
-        dt = clock.tick(60) / 1000
+    def __init__(self) -> None:
+        pygame.init()
+        screen: pygame.Surface = pygame.display.set_mode((1920, 1080))
+        clock: pygame.time.Clock = pygame.time.Clock()
+        running: bool = True
+        background: pygame.Surface = pygame.Surface((1920, 1080))
+        pygame.draw.rect(background, (0, 0, 0), background.get_rect())
+        player = Player()
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            screen.blit(background, (0, 0))
+            screen.blit(player.image, player.rect)
+            player.check_input()
+            pygame.display.flip()
+            clock.tick(60)
 
     pygame.quit()
