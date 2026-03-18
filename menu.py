1adafa709f68202b5a0a8b3c61c1b8066bab151e
schmidtnandor <schmidt.nandor@students.jedlik.eu>
2026-03-18 08:42:14 +0100

    menu design finished

--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 """Game menu entry point."""
+
 # pylint: disable=invalid-name,no-member,too-few-public-methods
 
 import pygame
@@ -8,19 +9,24 @@ from Game import Game
 
 
 class Menu:
-    """Presents main menu and launches Game."""
     def __init__(self):
         pygame.init()
         pygame.display.set_mode((1200, 800))
         menu_surface = pygame.display.get_surface()
-        menu = pygame_menu.Menu(
-            "Welcome", 1200, 800, theme=pygame_menu.themes.THEME_GREEN
+
+        custom_theme = pygame_menu.Theme(
+            background_color=(0, 0, 0),
+            title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_NONE,
+            widget_font=pygame_menu.font.FONT_MUNRO,
         )
-        menu.add.button("Play", self.start_game)
-        menu.add.button("Quit", pygame_menu.events.EXIT)
+        menu = pygame_menu.Menu(title="", width=1200, height=800, theme=custom_theme)
+
+        menu.add.label("SPACE INVADERS", font_size=100, font_color=(255, 255, 255))
+        menu.add.image("grafika/enemy.png", scale=(1.0, 1.0))
+        menu.add.button("Play", self.start_game, font_size=50)
+        menu.add.button("Quit", pygame_menu.events.EXIT, font_size=50)
         menu.mainloop(menu_surface)
 
     def start_game(self):
-        """Launch the main game loop when Play is selected."""
         game = Game()
         game.run()
